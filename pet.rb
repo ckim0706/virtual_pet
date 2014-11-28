6bfc4f14e3bead07d8e4ec847c88b6f60bd84499
Charles Kim <ckim0706@gmail.com>
2014-11-28 12:06:07 +0900

    creation of pet.rb

---
+++ pet.rb
@@ -0,0 +1,31 @@
+# Ranger Rick
+# Pseudocode
+# 
+
+class Person
+  #
+  # collection of rewards
+  # gets.chomp to receive commands
+  # commands for the racoon
+  # - feed
+  # - tell raccoon to sleep
+  # - give trophy
+  # - play catch/fetch
+end
+
+class Raccoon
+  # hunger level (case 1~5) + energy level(?)
+  # play status
+  # sleep status
+  # skill_level
+  # rate of metabolism method --> passage of time
+  # eat
+  # sleep
+  # level up
+  # play (catch)
+end
+
+class Trophy
+  # name
+end
+
